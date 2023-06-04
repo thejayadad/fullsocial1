2473d35a8e8a9694be527ca7b7271b9a392400bf
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-04 17:33:55 -0400

    update

--- social/src/app/post/[id]/page.js
+++ social/src/app/post/[id]/page.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AiFillDelete, AiFillLike, AiOutlineLike } from 'react-icons/ai'
+import { BsFillPencilFill } from 'react-icons/bs'
+
 
 const PostDetail = () => {
   return (
@@ -6,16 +9,17 @@ const PostDetail = () => {
     <h1 class="title-font text-center text-lg font-medium text-gray-900 mb-1">Post Title</h1>
     <h2 class="tracking-widest text-center text-xs title-font font-medium text-gray-400 mb-1">Date</h2>
     <h2 class="tracking-widest text-center text-xs title-font font-medium text-gray-900  mb-1">By Dunbar</h2>
-    <div className='text-center'>
+    <div className='text-center border-gray-200'>
     <button
-    className="text-center inline-flex text-black items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-    >Edit Post</button>
+    className="text-center inline-flex border-gray-200 items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 mb-4"
+    ><BsFillPencilFill/> Edit</button>
         <button
-    className="text-center inline-flex text-black items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-    >Delete</button>
+    className="text-center inline-flex border-gray-200 items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 mb-4"
+    ><AiFillDelete /> Delete
+    </button>
     </div>
     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-screen w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog" />
+          <img class="lg:h-screen w-full object-cover object-center" src="https://images.pexels.com/photos/1868991/pexels-photo-1868991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="blog" />
           <div class="p-6">
             <div className='flex justify-between'>
             <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 mr-4">CATEGORY</h2>
@@ -30,16 +34,11 @@ const PostDetail = () => {
             </p>
             <div class="flex items-center flex-wrap ">
     
-              <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                  <circle cx="12" cy="12" r="3"></circle>
-                </svg>1.2K
-              </span>
-              <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>6
+            <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-gray-200">
+                <button class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                <AiFillLike  size={16}  /> 
+                {/* <AiOutlineLike size={20} onClick={handleLike} /> */}
+                </button>1.2K
               </span>
             </div>
           </div>
--- social/src/components/Navbar.js
+++ social/src/components/Navbar.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => {
     const user = false
   return (
-    <header className="body-font text-black">
+    <header className="body-font text-black border-b border-gray-200 p-8">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <Link href={"/"} className="flex title-font font-medium items-center text-black mb-4 md:mb-0">
     <span className="ml-3 text-xl">Blog Site</span>
