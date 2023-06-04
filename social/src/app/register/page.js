6298f8c60a61a938431088f209c37265fe3e24d6
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-04 17:45:07 -0400

    update

--- social/src/app/register/page.js
+++ social/src/app/register/page.js
@@ -17,7 +17,7 @@ const Register = () => {
      <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
      <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
    </div>
-   <button class="text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">Login</button>
+   <button class="text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">Register</button>
  </div>
 </section>
   )
--- social/src/components/Navbar.js
+++ social/src/components/Navbar.js
@@ -5,24 +5,11 @@ const Navbar = () => {
     const user = false
   return (
     <header className="body-font text-black border-b border-gray-200 p-8">
-    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <Link href={"/"} className="flex title-font font-medium items-center text-black mb-4 md:mb-0">
+    <div className="container flex justify-between items-center m-auto max-w-screen-md align-center">
+    <Link href={"/"} className="flex title-font font-medium items-center text-black">
     <span className="ml-3 text-xl">Blog Site</span>
     </Link>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center space-x-5">
-          <Link 
-          className="inline-flex text-black items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-          href={"/"}>Home</Link>
-          <Link 
-          className="inline-flex text-black items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-          href={"/blogs"}>Blogs</Link>
-          <Link 
-          className="inline-flex text-black items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-          href={"/about"}>About</Link>
-          <Link 
-           className="inline-flex text-black items-center bg-[#fff] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-          href={"/"}>Contact</Link>
-        </nav>
+    <div>
         {user ? (
           <>
           <Link
@@ -56,6 +43,7 @@ const Navbar = () => {
           </Link>
           </>
         )}
+        </div>
     </div>
     </header>
   )
